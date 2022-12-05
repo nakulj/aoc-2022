0ea8e3496a2ff4897b6683155111af97bebd146a
nakulj <3213360+nakulj@users.noreply.github.com>
2022-12-05 01:15:27 -0800

    day 1 pt 2

--- day1.py
+++ day1.py
@@ -1,14 +1,14 @@
 import sys
 
 if __name__ == "__main__":
+  max_cals = []
   cals = 0
-  max_cals = 0
   with open(sys.argv[1]) as f:
     for line in f:
       if line == '\n':
-        max_cals = max(max_cals, cals)
+        max_cals = sorted(max_cals + [cals])[-3:]
         cals = 0
       else:
         cals += int(line)
-    max_cals = max(max_cals, cals)
-  print(max_cals)
+    max_cals = sorted(max_cals + [cals])[-3:]
+  print(sum(max_cals))
